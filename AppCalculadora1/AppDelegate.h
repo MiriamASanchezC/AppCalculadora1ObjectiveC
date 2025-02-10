dfe02333c506b9b72466368297d027fec1f7d4f0
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-10 17:28:39 -0600

    Initial Commit

---
+++ AppCalculadora1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AppCalculadora1
+//
+//  Created by Miriam Sanchez on 10/02/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
