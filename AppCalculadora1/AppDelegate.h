698f0203a9d35d6034eb393400ee7c3eab0b1f72
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-19 21:27:35 -0600

    calculadora1

--- AppCalculadora1/AppDelegate.h
+++ AppCalculadora1/AppDelegate.h
@@ -9,6 +9,21 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+@property (weak) IBOutlet NSTextField *txtOp;
+@property (weak) IBOutlet NSTextFieldCell *txtIgual;
+
+@property (weak) IBOutlet NSTextField *txtResul;
+@property (weak) IBOutlet NSTextField *txtNum1;
+
+@property (weak) IBOutlet NSTextField *txtNum2;
+- (IBAction)btnSuma:(id)sender;
+
+- (IBAction)btnResta:(id)sender;
+
+- (IBAction)btnMultiplica:(id)sender;
+
+- (IBAction)btnDivide:(id)sender;
+
 
 @end
 
--- AppCalculadora1/AppDelegate.m
+++ AppCalculadora1/AppDelegate.m
@@ -13,6 +13,7 @@
 @end
 
 @implementation AppDelegate
+@synthesize txtOp,txtNum1,txtNum2,txtIgual,txtResul; //nos permite utilizar las variables
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
@@ -29,4 +30,35 @@
 }
 
 
+- (IBAction)btnDivide:(id)sender {
+    float a = [txtNum1 floatValue];
+    float b = [txtNum2 floatValue];
+    float c = a / b;
+    txtOp.stringValue = @" / ";
+    txtResul.stringValue = [NSString stringWithFormat:@"%f", c];
+}
+
+- (IBAction)btnMultiplica:(id)sender {
+    float a = [txtNum1 floatValue];
+    float b = [txtNum2 floatValue];
+    float c = a * b;
+    txtOp.stringValue = @" * ";
+    txtResul.stringValue = [NSString stringWithFormat:@"%f", c];
+}
+
+- (IBAction)btnResta:(id)sender {
+    float a = [txtNum1 floatValue];
+    float b = [txtNum2 floatValue];
+    float c = a - b;
+    txtOp.stringValue = @" - ";
+    txtResul.stringValue = [NSString stringWithFormat:@"%f", c];
+}
+
+- (IBAction)btnSuma:(id)sender {
+    float a = [txtNum1 floatValue];
+    float b = [txtNum2 floatValue];
+    float c = a + b;
+    txtOp.stringValue = @" + ";
+    txtResul.stringValue = [NSString stringWithFormat:@"%f", c];
+}
 @end
